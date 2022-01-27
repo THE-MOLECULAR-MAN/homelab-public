3e9c4562c74e801d6c980f58f84a9a9c01f882d7
THE-MOLECULAR-MAN <99990915+THE-MOLECULAR-MAN@users.noreply.github.com>
2022-01-26 22:43:52 -0500

    migrated from private repo

---
+++ windows/restart_all_running_services.ps1
@@ -0,0 +1,9 @@
+﻿# Tim H 2019
+# Powershell script to restart all of the services that are currently running
+foreach ($svc in Get-Service){
+  if($svc.Status -eq "Running") {
+    Write-Output $svc.DisplayName
+    # Restart-Service -Force $svc.name   # Don't do this, results in "Can't accept control messages" errors
+    Restart-Service $svc.name
+  }
+}
---
+++ windows/restart_vmware_services.ps1
@@ -0,0 +1,9 @@
+﻿# Tim H 2019
+# Powershell script
+# Restart all VMware related services in Windows
+foreach ($svc in Get-Service){
+  if(($svc.displayname.StartsWith("VMware")) -AND ($svc.Status -eq "Running")) {
+    Write-Output $svc.DisplayName
+    Restart-Service -Force $svc.name
+  }
+}
