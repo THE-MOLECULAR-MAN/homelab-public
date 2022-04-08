78bc80bddabee5f517f3e06330470e77f831483f
THE-MOLECULAR-MAN <99990915+THE-MOLECULAR-MAN@users.noreply.github.com>
2022-04-08 11:14:45 -0400

    additional comments

--- aws/api-gateway-http-to-sns-test.sh
+++ aws/api-gateway-http-to-sns-test.sh
@@ -8,17 +8,20 @@
 
 # TODO: load these from from .env file
 AWS_REGION="us-east-1"
-AWS_ARN="arn:aws:sns:$AWS_REGION:REDACTED:REDACTED"
-API_ID="REDACTED"                   # sensitive since no auth
+SNS_ARN="arn:aws:sns:$AWS_REGION:REDACTED:REDACTED" # ARN of the SNS topic to send a message to
+API_ID="REDACTED"                   # AWS API Gateway ID; sensitive since no auth
+
 #API_GATEWAY_KEY="REDACTED"         # This cloud formation stack disables the requirement for the API key, not sure how to reenable it after the stack has been deployed
 
 # URL as defined by API gateway in this CloudFormation Stack
-BASE_URL="https://$API_ID.execute-api.$AWS_REGION.amazonaws.com/Prod/stats?topic=$AWS_ARN"
+BASE_URL="https://$API_ID.execute-api.$AWS_REGION.amazonaws.com/Prod/stats?topic=$SNS_ARN"
 
+# Basic curl test to AWS's API Gateway
 curl --location --request POST "$BASE_URL" \
     --header 'Content-Type: text/plain' \
     --data-raw 'Curl test 1'
 
+# Testing to Rapid7's InsightConnect Webhook instead
 # Posting to InsightConnect WebHook API
 curl -vv -X POST -H "Content-Type: application/json; charset=utf-8" \
     -d '{"notification_description":"test2"}' \
