7ca678dbba475c747683b660f99d97862b88a1eb
thonker-r7 <tim@private.local>
2022-05-17 12:49:04 -0400

    fixed journal issues, removed confirmation prompts

--- linux/centos7-clean-free-space.sh
+++ linux/centos7-clean-free-space.sh
@@ -6,8 +6,11 @@
 ##############################################################################
 
 # clean up the history of users
-sudo journalctl --vacuum-time=1h
-sudo journalctl --vacuum-size 1M
+# https://www.linuxuprising.com/2019/10/how-to-clean-up-systemd-journal-logs.html
+sudo journalctl --disk-usage
+sudo journalctl --vacuum-size=1K
+# not all versions of journalctl recognize the --rotate flag
+sudo journalctl --disk-usage
 
 # clean up yum caches
 yum clean all
@@ -17,10 +20,11 @@ package-cleanup --quiet --leaves
 package-cleanup --quiet --leaves | xargs yum remove -y
 
 # remove old, unused Kernels, keep only the most recent 2 kernels
-package-cleanup --oldkernels --count=2
+package-cleanup -y --oldkernels --count=2
 
 # clear out logs but don't delete the files
 find /var -name "*.log" -exec truncate {} --size 0 \;
+find /var/log -type f -exec truncate {} --size 0 \;
 
 # delete some cache files/directories
 rm -rf /root/.composer/cache
@@ -30,4 +34,6 @@ rm -rf /var/cache/yum
 rm -rf /var/tmp/yum-*
 
 # begin the process of trimming the SSD (may take a long time in the background)
+# the fstrim command should be after all the deletes and at the bottom
+# of this script
 fstrim --verbose --all
