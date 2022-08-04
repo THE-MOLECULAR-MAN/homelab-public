7e4992d2082fdf779fe659d2a671fbbd8f8acae3
THE-MOLECULAR-MAN <99990915+THE-MOLECULAR-MAN@users.noreply.github.com>
2022-08-04 09:44:29 -0400

    updated to splunk 9, renamed

--- splunk/install-upgrade-splunk-centos7.sh
+++ splunk/install-upgrade-splunk-centos7.sh
@@ -13,9 +13,10 @@
 
 set -e
 
+# don't try to migrate the "2.6" into the os variable. doesn't work.
 product="splunk"       # values can be : splunk , splunkforwarder
-version="8.2.4"        # Splunk product Version
-hash="87e2dda940d1"    # specific per Version
+version="9.0.0.1"      # Splunk product Version
+hash="9e907cedecb1"    # hash of specific build, changes with every version.
 arch="x86_64"          # values can be : x86_64 (redhat, tgz), amd64 (ubuntu), x64 (Windows)
 os="linux"             # values can be : linux, windows
 pkg="rpm"              # Values can be : tgz, rpm, deb, msi
@@ -26,23 +27,21 @@ filename="${product}-${version}-${hash}-${os}-2.6-${arch}.${pkg}"
 wget "https://download.splunk.com/products/splunk/releases/${version}/${os}/${filename}"
 wget "https://download.splunk.com/products/splunk/releases/${version}/${os}/${filename}.md5"
 
-# example valid download URL:
+# example valid download URLs:
 # https://download.splunk.com/products/splunk/releases/8.2.4/linux/splunk-8.2.4-87e2dda940d1-linux-2.6-x86_64.rpm
+# https://download.splunk.com/products/splunk/releases/9.0.0.1/linux/splunk-9.0.0.1-9e907cedecb1-linux-2.6-x86_64.rpm
 
 # verify the integrity of the downloaded Linux installer
 md5sum --check "${filename}.md5"
 
-# Stop the splunk service (if running) before upgrading; this takes 30 seconds
-#	or more before it returns control to the command line
-# Unneccesary since the yum upgrade command will stop the service if it
-#	is running
-#sudo service splunk stop
-
 # install/upgrade the service
 #	this takes 1-2 minutes
+#   it will automatically stop the service if needed, no need to manually
+#   stop the splunk service
 sudo yum install -y ./"${filename}"
 
-# this prompts an INTERACTIVE prompt to accept license agreement and perform migration
+# Triggers INTERACTIVE prompt to accept license agreement and perform migration
+# Yes, the service status triggers it, not the install
 sudo service splunk status
 
 # start the service again if it wasn't already running
