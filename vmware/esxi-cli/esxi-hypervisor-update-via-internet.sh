e368950ebf3a3de1a6a56cc72e0b517f5bb0c2b2
THE-MOLECULAR-MAN <99990915+THE-MOLECULAR-MAN@users.noreply.github.com>
2022-08-14 23:53:08 -0500

    add automatic loop through and shut down all running VMs added lots of comments commented out optional stuff

--- vmware/esxi-cli/esxi-hypervisor-update-via-internet.sh
+++ vmware/esxi-cli/esxi-hypervisor-update-via-internet.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Tim H 2022
-# Update via internet
+# Update ESXi hypervisor via internet
+# Use a screen session on a jump host
 
 # references:
 #   https://www.altaro.com/vmware/top-20-esxcli-commands/
@@ -14,37 +15,47 @@ vmware -v
 esxcli system version get
 
 # list VIBs installed:
-esxcli software vib list
+# esxcli software vib list
 
 # list all virtual machines:
-vim-cmd vmsvc/getallvms
-
-# show running virtual machines:
-esxcli vm process list
+# vim-cmd vmsvc/getallvms
 
 # enable maintenance mode, be sure to undo it before reboot or it will prevent VMs from auto-starting after reboot
 esxcli system maintenanceMode set --enable true
 
+# show running virtual machines:
+esxcli vm process list
+
 # list of WorldIDs of running VMs
 # TODO: run this as a loop with a 30 sec sleep in between calls
-#esxcli vm process list | grep "World ID" | cut -d ' ' -f6
-
 # graceful shutdown of running VMs - use WorldID from previous command
-esxcli vm process kill --type=soft --world-id=1084026
+for ITER_WORLD_ID in $(esxcli vm process list | grep "World ID" | cut -d ' ' -f6); do
+    echo "Gracefully shutting down $ITER_WORLD_ID"
+    #esxcli vm process kill --type=soft --world-id="$ITER_WORLD_ID"
+    #sleep 30
+done
+echo "done shutting down VMs"
 
 # list running tasks
 vim-cmd vimsvc/task_list
 
-# at the time of this writing, the latest is below, dated 2022/03/29
-# this command takes a WHILE to run
+# list depot packages, verify connection and URL
+# takes about 40 seconds to run
+# esxcli software sources profile list --depot=https://hostupdate.vmware.com/software/VUM/PRODUCTION/main/vmw-depot-index.xml
+LATEST_PROFILE=$(esxcli software sources profile list --depot=https://hostupdate.vmware.com/software/VUM/PRODUCTION/main/vmw-depot-index.xml | grep '\-standard' | tail -n1 | cut -d ' ' -f1)
+
+# this command takes a WHILE to run - 5-10 minutes
 # maybe consider adding a beep afterward?
 # TODO: consider addding && disable main && reboot, maybe some sleeps too
-esxcli software profile update --profile=ESXi-7.0U3d-19482537-standard --depot https://hostupdate.vmware.com/software/VUM/PRODUCTION/main/vmw-depot-index.xml
+# the last item in the previous command is usually the latest, but go for
+# the standard version, no the no-tools version.
+# this command blocks until the update is done
+esxcli software profile update --profile="$LATEST_PROFILE" --depot https://hostupdate.vmware.com/software/VUM/PRODUCTION/main/vmw-depot-index.xml
 
 # the ESX equivalent of "top" to watch the most CPU-intensive processes
 # https://kb.vmware.com/s/article/2001448
 # gotta define that env variable first, otherwise it doesn't work
-TERM=xterm esxtop
+# TERM=xterm esxtop
 
 # see if the update is still running:
 ps -Tcgi | grep "esxcli software profile update"
@@ -52,8 +63,9 @@ ps -Tcgi | grep "esxcli software profile update"
 # disable maintenance mode so VMs will come back up on reboot
 esxcli system maintenanceMode set --enable false
 
-# have to reboot, just use regular Linux command. No need for special ESX command
-reboot now
+# have to reboot, just use regular Linux command. No need for special ESX 
+# command. it doesn't immediately logout, so do it manually
+reboot now && logout
 
 # check version after reboot
 vmware -v
