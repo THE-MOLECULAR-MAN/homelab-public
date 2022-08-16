f1ad50a040e78c937862b0f2623575ca543b7d32
THE-MOLECULAR-MAN <99990915+THE-MOLECULAR-MAN@users.noreply.github.com>
2022-08-15 22:44:23 -0500

    sudo fixes, comments

--- velociraptor/install-velociraptor-agent-osx-persistent.sh
+++ velociraptor/install-velociraptor-agent-osx-persistent.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 # Tim H 2022
-
+# Pre-reqs: download the client.config.yaml file from the server and
+# place it in the local directory
+#
 ##############################################################################
 #   STEP 4:
 #       INSTALLING THE VELOCIRAPTOR AGENT ON OS X AND MAKING IT AUTOSTART
 ##############################################################################
-# OS X - sudo is required
+
+if [ ! -f "client.config.yaml" ]; then
+    echo "client.config.yaml does not exist, cannot proceed.
+    exit 1"
+fi
+
+VELOC_OSX_BINARY_URL=$(curl -s https://api.github.com/repos/velocidex/velociraptor/releases/latest | jq --raw-output 'limit(1 ; ( .assets[].browser_download_url | select ( contains("darwin") )))')
+VELOC_OSX_BINARY_BASENAME=$(basename "$VELOC_OSX_BINARY_URL")
+wget --quiet "$VELOC_OSX_BINARY_URL" "$VELOC_OSX_BINARY_URL.sig"
+gpg --verify "$VELOC_OSX_BINARY_BASENAME.sig"
 
 # install it
-sudo velociraptor --config client.config.yaml service install
+sudo ./"$VELOC_OSX_BINARY_BASENAME" --config client.config.yaml service install
 
 # see if process is running:
 pgrep --list-full "velociraptor"
--- velociraptor/install-velociraptor-server-on-ubuntu.sh
+++ velociraptor/install-velociraptor-server-on-ubuntu.sh
@@ -9,6 +9,7 @@
 # def create the 1 GB safety file since disk may fill up
 # a bunch of the documentation talks about how the server could fill up the
 # disk space
+fallocate -l 1G space_saver_safe_to_delete
 
 # this is slow:
 # installs it and starts the process, creates an autostarting process on next
@@ -25,27 +26,34 @@ service velociraptor_server status
 tree -apu /opt/velociraptor
 
 # test the env path, verify it can run
-velociraptor version
+# needs to be sudo to read the config file
+sudo velociraptor version
 
 # check for listening ports:
-ss -antpl | grep velociraptor
+# needs to be sudo or run as root user:
+sudo ss -antpl | grep velociraptor
 
 # open the firewall:
-/bin/systemctl start firewalld.service
-sudo firewall-cmd --zone=public --add-port=8889/tcp --permanent
-sudo firewall-cmd --zone=public --add-port=8000/tcp --permanent
-sudo firewall-cmd --zone=public --add-port=8001/tcp --permanent
-sudo firewall-cmd --zone=public --add-port=8003/tcp --permanent
-sudo firewall-cmd --reload
+# firewall is disabled by default on Ubuntu 20.04 EC2 instances
+# https://www.digitalocean.com/community/tutorials/how-to-set-up-a-firewall-with-ufw-on-ubuntu-20-04
+# sudo /bin/systemctl start firewalld.service
+# sudo firewall-cmd --zone=public --add-port=8889/tcp --permanent
+# sudo firewall-cmd --zone=public --add-port=8000/tcp --permanent
+# sudo firewall-cmd --zone=public --add-port=8001/tcp --permanent
+# sudo firewall-cmd --zone=public --add-port=8003/tcp --permanent
+# sudo firewall-cmd --reload
 
 # test the HTTP server
 # curl command must use FQDN, not just localhost:
 curl --insecure -v -u "admin:admin" "https://$(hostname):8889/app/index.html"
 
 # generate the client config file:
-/usr/local/bin/velociraptor --config /etc/velociraptor/server.config.yaml \
-    config client > /etc/velociraptor/client.config.yaml
+sudo /usr/local/bin/velociraptor --config /etc/velociraptor/server.config.yaml \
+    config client | sudo tee /etc/velociraptor/client.config.yaml
 sudo chmod +r /etc/velociraptor/client.config.yaml
 
 # get that file back onto your laptop, you'll need it for anywhere
 # you want to install the agent
+
+# now visit this page in a web browser, replacing the host with your hostname
+# https://velociraptor.local:8889/app/index.html?#/search/all
