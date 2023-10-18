cff2bbe65fd93fb251d1042399612b27c97402c8
THE-MOLECULAR-MAN <99990915+THE-MOLECULAR-MAN@users.noreply.github.com>
2023-10-18 15:19:09 -0400

    lint fixes

--- cisco-umbrella/cisco-supported-dynamic-dns-installer-linux.sh
+++ cisco-umbrella/cisco-supported-dynamic-dns-installer-linux.sh
@@ -6,17 +6,13 @@
 sudo apt-get update
 sudo apt-get -y install ddclient
 
-
-/etc/ddclient.conf
-
-##
+## contents of /etc/ddclient.conf
 ## OpenDNS.com account-configuration
 ##
-protocol=dyndns2
-use=web, web=myip.dnsomatic.com
-ssl=yes
-server=updates.opendns.com
-login=opendns_username
-password='opendns_password'
-opendns_network_label
-
+# protocol=dyndns2
+# use=web, web=myip.dnsomatic.com
+# ssl=yes
+# server=updates.opendns.com
+# login=opendns_username
+# password='opendns_password'
+# opendns_network_label
