7ee7444655a35c90ab0035a0b4ef6319e7744e08
THE-MOLECULAR-MAN <99990915+THE-MOLECULAR-MAN@users.noreply.github.com>
2023-10-18 15:20:53 -0400

    comments for future lint

--- cisco-umbrella/verify-bittorrent-tracker-list.sh
+++ cisco-umbrella/verify-bittorrent-tracker-list.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Tim H 2022
+#
 # Turns out this doesn't work at all b/c it's not a standard HTTP request and some of them use UDP instead
 # can't rely on basic CURL commands, each protocol is different
 # 
-
-# 
-#TRACKER_LIST_FILE_PATH="$HOME/Desktop/trackers2.txt"
+# TRACKER_LIST_FILE_PATH="$HOME/Desktop/trackers2.txt"
 
 TRACKER_LIST_FILE_PATH="./trackers_list_https.txt"
 
+# TODO: replace with while read loop
 for url in $(cat "$TRACKER_LIST_FILE_PATH")
 do 
   echo "Testing: $url ..."
