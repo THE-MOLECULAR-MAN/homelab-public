1d64351e8e178a6bf07efef4a3d89b18d5ec0638
THE-MOLECULAR-MAN <99990915+THE-MOLECULAR-MAN@users.noreply.github.com>
2024-03-14 09:17:53 -0400

    updated to Ventura

--- osx/create-bootable-osx-installer.sh
+++ osx/create-bootable-osx-installer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Tim H 2022
+# Tim H 2022, 2024
 # create bootable flash drive for OSX Big Sur (macOS 11.x) installer
 # https://support.apple.com/en-mide/HT201372
 # latest OS X version based on hardware SKU:
@@ -11,6 +11,25 @@
 # Step 3: run this command and replace the last part with the mount point for
 #  your flash drive
 
-sudo /Applications/Install\ macOS\ Big\ Sur.app/Contents/Resources/createinstallmedia --volume /Volumes/silverFlash
-sudo diskutil unmount "/Volumes/Install macOS Big Sur"
+# find available installers:
+find /Applications -type f -name 'createinstallmedia'
 
+# list flash drives:
+diskutil list
+
+# flash drive needs to be "formatted as Mac OS Extended" (journaled)
+# seems like the partition table needs to be Apple Partition Map
+
+# create the installer on the flash drive
+sudo /Applications/Install\ macOS\ Ventura.app/Contents/Resources/createinstallmedia --volume /Volumes/f1
+
+# it will rename the mount point, need to unmount before removing it
+sudo diskutil unmount "/Volumes/Install macOS Ventura"
+
+
+
+# Next steps:
+# 1) boot up Macbook Pro and hold the Option button
+# 2) Boot into the Disk Manager
+# 3) Format the local HDD with the GUID Partition Table schema
+# 4) Start the installer
