6f0d257defe468b39d1873fc3ebe489913ed6ca2
THE-MOLECULAR-MAN <99990915+THE-MOLECULAR-MAN@users.noreply.github.com>
2024-05-22 10:53:52 -0400

    added fix/workaround for arch

--- gaming/steam-deck/steamdeck-wireguard-vpn.sh
+++ gaming/steam-deck/steamdeck-wireguard-vpn.sh
@@ -6,8 +6,7 @@
 # from my laptop:
 scp ~/Downloads/wireguard_vpn_steamdeck.conf deck@10.0.1.61:~/
 
-
-
+#####################################################################
 # on the steam deck via SSH:
 ssh deck@10.0.1.61
 # start a screen session for all this:
@@ -18,17 +17,17 @@ sudo steamos-readonly disable
 # install updates, interactive
 sudo pacman -Syyu
 
-
 sudo pacman -S --needed wireguard-tools 
-
 
 sudo cp ~/wireguard_vpn_steamdeck.conf /etc/wireguard/wg0.conf
 
 # edit the config file and replace the static public IP with the DDNS:
 sudo vim /etc/wireguard/wg0.conf
 
+# workaround for Arch
+sudo ln -s /usr/bin/resolvectl /usr/local/bin/resolvconf
+
 # start the VPN, test it
 sudo wg-quick up wg0
-
 
 sudo steamos-readonly enable
