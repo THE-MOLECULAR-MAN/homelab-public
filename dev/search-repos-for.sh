66c184703c7bd8c6ebb6cd08b1d7eeee2c37e542
THE-MOLECULAR-MAN <99990915+THE-MOLECULAR-MAN@users.noreply.github.com>
2024-05-22 10:58:08 -0400

    added search for python files and jupyter notebooks, limited output length

--- dev/search-repos-for.sh
+++ dev/search-repos-for.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-# Tim H 2021
-# READY_FOR_PUBLIC_REPO
+# Tim H 2021-2024
 # Search my source code repos for something
 
 # for some reason (GDrive mounting or symlink?) have to cd
 #    there first, can't just search directly
-cd "$HOME/source_code" || exit 1
 
-find . -type f -iname '*.sh' -exec grep -i --color=always \
-    --with-filename "$1" {} \;
+cd "$HOME/source_code" || exit 1 
+
+find . -type f \
+    \( -name '*.sh' -o -name '*.py' -o -name '*.ipynb' \) \
+    -not -path '*.venv*' -not -path '*third_party*' \
+    -exec grep -i --color=always --with-filename "$1" {} \+ | grep -v 'image/png' | cut -c1-200
+
+# for troubleshooting:
+    # -exec grep -i --files-with-matches "$1" {} \+
