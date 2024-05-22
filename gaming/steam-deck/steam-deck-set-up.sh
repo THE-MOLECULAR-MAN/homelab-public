d47eae91789e5c37e0b79580d96e145f2f4743e6
THE-MOLECULAR-MAN <99990915+THE-MOLECULAR-MAN@users.noreply.github.com>
2024-05-22 10:52:29 -0400

    removed homebrew stuff, fixed sudo stuff

--- gaming/steam-deck/steam-deck-set-up.sh
+++ gaming/steam-deck/steam-deck-set-up.sh
@@ -2,7 +2,7 @@
 # Tim H 2022
 # Setting up a Steam Deck
 
-# first time only
+# First time set up using Konsole
 # passwd
 
 # almost always first command:
@@ -11,23 +11,27 @@ sudo steamos-readonly disable
 sudo systemctl start sshd 
 sudo systemctl enable sshd
 
+
+
+######### Can do over SSH now, preferably in a screen session
+
 # install and configure Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/deck/.bash_profile
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/deck/.bash_profile
 
 # shellcheck disable=SC2016
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/deck/.bash_profile
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+#echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/deck/.bash_profile
+#eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 sudo pacman-key --init
 
-# Temporary fix required Jan 2024
+# Temporary fix still required April 2024
 sudo vim /etc/pacman.conf
 # change SigLevel = TrustAll
-sudo pacman -S holo-keyring archlinux-keyring
+sudo pacman --noconfirm -S holo-keyring archlinux-keyring
 
 
-sudo pacman -S base-devel
-brew install gcc
+sudo pacman --noconfirm -S base-devel gcc screen
+# brew install gcc
 
 cd ~/Desktop || exit 1
 wget "https://www.emudeck.com/EmuDeck.desktop"
