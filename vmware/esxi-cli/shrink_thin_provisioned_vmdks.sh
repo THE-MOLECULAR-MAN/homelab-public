ea0d08b86298cf0a83c1abc6efec4aba9198caf2
THE-MOLECULAR-MAN <>
2025-06-08 15:41:02 -0400

    proper cleanup of thin files

--- vmware/esxi-cli/shrink_thin_provisioned_vmdks.sh
+++ vmware/esxi-cli/shrink_thin_provisioned_vmdks.sh
@@ -1,26 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 # Tim H 2025
+# for shrinking virtual disks in VMware ESXi v7 command line that have thin provisioned disks
 
+# ssh root@10.0.1.13
+# ssh root@10.0.1.27
 
+# esxcli does not have screen
+# screen -S shrink_vmdks -d -m -L -Logfile /var/log/shrink_vmdks.log
 
-# for shrinking virtual disks in VMware ESXi v7 that have thin provisioned disks
+# set -euo pipefail
 
-ssh root@10.0.1.13
+cd /vmfs/volumes
 
-screen -S shrink_vmdks -d -m -L -Logfile /var/log/shrink_vmdks.log
+# find /vmfs/volumes/r7-3000-raid10/ -type f -name 'vmware*.log' -exec rm -f "{}" \;
 
-# 1.4 TB used before shrinking
-
-set -euo pipefail
-
-find . -type f -name '*.vmdk' -exec sh -c '
-  for vmdk; do
-    echo "Shrinking $vmdk ..."
-    vmkfstools --punchzero "$vmdk"
-    echo "Finished shrinking $vmdk ."
+find . -type f -name '*.vmdk' ! -name '*-flat.vmdk' -exec sh -c '
+  for virtual_disk_filepath in "$@"; do
+    vmkfstools --punchzero "$virtual_disk_filepath" || continue
+    thin_temp_filename="${virtual_disk_filepath%.vmdk}-thin.vmdk"
+    vmkfstools -i "$virtual_disk_filepath" -d thin "$thin_temp_filename" || continue
+    thick_filename="${virtual_disk_filepath%.vmdk}-orig.vmdk"
+    mv "$virtual_disk_filepath" "$thick_filename" || continue
+    mv "$thin_temp_filename" "$virtual_disk_filepath" || continue
   done ' sh {} +
 
 echo "Finished shrinking all VMDKs."
-
-# vmkfstools --punchzero "/vmfs/volumes/datastore1/MyVM/MyVM.vmdk"
-# [r7-3000-raid10] dss2/dss2.vmdk
