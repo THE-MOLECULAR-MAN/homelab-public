f0a072a8fe8d524a8204c84b5e2312a47f5280bc
THE-MOLECULAR-MAN <>
2025-06-08 15:38:24 -0400

    migrated things from Ubuntu cleanup

--- linux/centos7-clean-free-space.sh
+++ linux/centos7-clean-free-space.sh
@@ -7,24 +7,24 @@
 
 # clean up the history of users
 # https://www.linuxuprising.com/2019/10/how-to-clean-up-systemd-journal-logs.html
-sudo journalctl --disk-usage
-sudo journalctl --vacuum-size=1K
-# not all versions of journalctl recognize the --rotate flag
-sudo journalctl --disk-usage
+journalctl --rotate
+journalctl --flush
+journalctl --sync 
+journalctl --vacuum-size=1
 
 # clean up yum caches
 yum clean all
 
 # remove unused Yum packages
-sudo package-cleanup --quiet --leaves 
 sudo package-cleanup --quiet --leaves | xargs yum remove -y
 
 # remove old, unused Kernels, keep only the most recent 2 kernels
 sudo package-cleanup -y --oldkernels --count=2
 
 # clear out logs but don't delete the files
-find /var -name "*.log" -exec truncate {} --size 0 \;
-find /var/log -type f -exec truncate {} --size 0 \;
+find /var/log -type f -name "*log" -exec truncate "{}" --size 0 \;
+find /var/log -type f -name "*.gz" -delete
+find /var/log -type f \( -name "*.log.[0-9]" -o -name "*.[0-9].log" -o -name "*.[0-9]" \) -delete
 
 # delete some cache files/directories
 rm -rf /root/.composer/cache
@@ -32,7 +32,19 @@ rm -rf /home/*/.composer/cache
 rm -rf /home/*/.cache/*/* /root/.cache/*/* 
 rm -rf /var/cache/yum
 rm -rf /var/tmp/yum-*
+rm -rf /tmp/* /var/tmp/*
 
+# for VMware ESXi virtual machines, shrink the VMDK
+dd if=/dev/zero of=/zerofile bs=1M status=progress || true
+sync
+rm -f /zerofile
+sync
+echo
+echo "=== Cleanup complete at $(date) ==="
+shutdown -h now
+
+
+# for MicroSD cards, we need to trim the SSD
 # begin the process of trimming the SSD (may take a long time in the background)
 # the fstrim command should be after all the deletes and at the bottom
 # of this script
