7ca0a44d5f150d58c37aaad420d4299790c203e6
THE-MOLECULAR-MAN <>
2025-08-15 10:06:58 -0500

    tmux session, reload config, flush to disk

--- gaming/steam-deck/steam-deck-updates.sh
+++ gaming/steam-deck/steam-deck-updates.sh
@@ -3,20 +3,24 @@
 
 # Steam Deck updates
 
-# start a screen session for all this:
-screen
+# start a tmux session for all this:
+tmux
 
 sudo steamos-readonly disable
 
+sudo steamosctl reload-config
+# sudo steamosctl set-max-charge-level 80
+
+# sudo pacman-key --init && sudo pacman-key --populate archlinux holo
 sudo pacman -Syyu
 
 # brew update && brew upgrade
 # brew doctor
 
 sudo flatpak update --noninteractive --assumeyes
-# flatpak update com.heroicgameslauncher.hgl
+flatpak update com.heroicgameslauncher.hgl
 
-# sudo flatpak install tv.plex.PlexHTPC tv.plex.PlexDesktop
+# sudo flatpak install --noninteractive --assumeyes tv.plex.PlexHTPC tv.plex.PlexDesktop
 
 # interactive only, doesn't take parameters, I looked thru all the src
 # exits on error but returns 0, seems to be fine, maybe doesn't clean up
@@ -24,6 +28,8 @@ sudo flatpak update --noninteractive --assumeyes
 
 sudo steamos-update
 # sudo steamos-update-os after-reboot
+
+sync
 
 sudo steamos-readonly enable
 
