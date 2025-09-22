0f0c9bae863b660ef2cd24462cc731c68569dbf0
THE-MOLECULAR-MAN <>
2025-09-22 08:27:21 -0400

    removed extraneous stuff, fixed linting

--- dev/update-all-git-repos.sh
+++ dev/update-all-git-repos.sh
@@ -18,34 +18,34 @@ fi
 cd "$PATH_TO_REPOS" || exit 3
 
 # delete thumbnails
-find . -type f -name '.DS_Store' -delete
+# find . -type f -name '.DS_Store' -delete
 
 # mark git hook scripts as executable
-find . -type f -path '*.git/hooks/*' ! -name '*.sample' -exec chmod u+x {} \+
+# find . -type f -path '*.git/hooks/*' ! -name '*.sample' -exec chmod u+x {} \+
 
 # mark .sh files as executable
 # the \+ is a lot faster than the \; in this situation
-find . -type f -name '*.sh' -exec chmod u+x {} \+
+# find . -type f -name '*.sh' -exec chmod u+x {} \+
 
 
 echo "Searching for git repositories..."
 # next line is touchy, be cautious about making changes
-find . -type d -name '.git' -print0 -maxdepth 3 | while read -r -d $'\0' ITER_PATH_TO_GIT_DIR
+find . -maxdepth 3 -mindepth 2 -type d -name '.git' -print0  | while read -r -d $'\0' ITER_PATH_TO_GIT_DIR
 do
 	# gotta have full path in here
 	cd "$(dirname "${PATH_TO_REPOS}"/"${ITER_PATH_TO_GIT_DIR}")" || exit 2
 	echo "${ITER_PATH_TO_GIT_DIR}"
-	git config core.fileMode true
+	# git config core.fileMode true
 	gh repo sync
 	# git pull
 
 	
 
-	# only do the status and push if it is one of my repos, skip if not
-	if [[ "${ITER_PATH_TO_GIT_DIR}" != *"third_party"* ]]; then
-  		git status #--ignored
-		git push
-	fi
+	# # only do the status and push if it is one of my repos, skip if not
+	# if [[ "${ITER_PATH_TO_GIT_DIR}" != *"third_party"* ]]; then
+  	# 	git status #--ignored
+	# 	git push
+	# fi
 	
 done
 
