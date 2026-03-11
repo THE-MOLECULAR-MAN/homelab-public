19afcc28f027500e159f08e897d2c8911d0eeb78
THE-MOLECULAR-MAN <99990915+THE-MOLECULAR-MAN@users.noreply.github.com>
2026-03-11 10:07:53 -0400

    homebrew added

--- osx/install_claude_code_macos.sh
+++ osx/install_claude_code_macos.sh
@@ -1,8 +1,51 @@
+##############################################################################
+# Installing Claude Code on MacOS
+##############################################################################
 curl -fsSL https://claude.ai/install.sh | bash
 echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.zshrc && source ~/.zshrc
   
 claude --version
 
+##############################################################################
+# Creating your first Claude Code project
+##############################################################################
 mkdir -p ~/example_claude_project
 cd ~/example_claude_project
 claude init
+
+##############################################################################
+# a better way to structure Claude projects
+##############################################################################
+mkdir -p ~/Claude_Code_Projects/
+cd ~/Claude_Code_Projects/
+
+# creating a new project:
+mkdir -p project1
+cd project1
+claude init
+
+
+##############################################################################
+# An even better way to structure projects
+##############################################################################
+
+
+
+##############################################################################
+# Making fancier apps with Claude Code - installing dependencies
+##############################################################################
+# Installing Homebrew - https://brew.sh/
+# You may need to install Xcode Command Line Tools first:
+# xcode-select --install
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# checking that Homebrew is installed correctly
+brew --version
+
+# Installing NodeJS and npm
+brew install node
+
+# checking that NodeJS and npm are installed correctly
+node --version
+npm --version
+
