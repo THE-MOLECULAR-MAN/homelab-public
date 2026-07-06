e7f2798c1466bb6d77ac7d07f81e85f82b2a12e8
THE-MOLECULAR-MAN <>
2026-07-06 10:23:09 -0400

    fixed dir issues

--- dev/update-all-git-repos.sh
+++ dev/update-all-git-repos.sh
@@ -18,7 +18,7 @@ fi
 cd "$PATH_TO_REPOS" || exit 3
 
 # delete thumbnails
-find . ! -path '*.git*' ! -path '*.venv*' ! -path '*third_party*' ! -path '*__pycache__*' ! -path './dataiku/*' -type f -name '.DS_Store' -delete
+find . ! -path '*.git*' ! -path '*.venv*' ! -path '*third_party*' ! -path '*__pycache__*' ! -path './dataiku_repos/*' -type f -name '.DS_Store' -delete
 # time find .  -type f -name '.DS_Store' -delete
 
 # mark git hook scripts as executable
@@ -33,29 +33,27 @@ gfind . -type f \
 	! -path '*.git*' \
 	! -path '*.claude*' \
 	! -path '*third_party*' \
-	! -path '*/dataiku/*' \
+	! -path './dataiku_repos/*' \
 	! -path '*.vscode*' \
 	! -path '*.ruff_cache*' \
 	\( -name '*.sh' -o -name '*.zsh' \) -print -exec chmod u+x {} \+
 
 echo "Searching for git repositories..."
 # next line is touchy, be cautious about making changes
-find . -maxdepth 3 -mindepth 2 -type d -name '.git' -print0 | while read -r -d $'\0' ITER_PATH_TO_GIT_DIR
+find . -maxdepth 4 -mindepth 2 -type d -name '.git' ! -path './third_party/*' ! -path './dataiku_repos/*' -print0 | while read -r -d $'\0' ITER_PATH_TO_GIT_DIR
 do
 	# gotta have full path in here
 	cd "$(dirname "${PATH_TO_REPOS}"/"${ITER_PATH_TO_GIT_DIR}")" || exit 2
-	
-	# only do the status and push if it is one of my repos, skip if not
-	if [[ "${ITER_PATH_TO_GIT_DIR}" != *"third_party"* ]]; then
-		echo -e "\n\n=== Syncing repo: $ITER_PATH_TO_GIT_DIR ===\n"
-		# quietly sync, do not display anything if sync was successful
-		if ! gh repo sync > /dev/null; then
-			echo "Error syncing: ${ITER_PATH_TO_GIT_DIR}"
-			git status # --ignored
-		fi
 
-		git stash list
+	echo -e "\n\n=== Syncing repo: $ITER_PATH_TO_GIT_DIR ===\n"
+	# quietly sync, do not display anything if sync was successful
+	if ! gh repo sync > /dev/null; then
+		echo "Error syncing: ${ITER_PATH_TO_GIT_DIR}"
+		git status # --ignored
 	fi
+
+	git stash list
+
 done
 
 echo "Script finished successfully."
